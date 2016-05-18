d76bf81c724db4968cbb4a15eb4c1a409b0e047b
wyb1995 <wy_b1995@163.com>
2016-05-18 17:45:29 +0800

    1

---
+++ src/fibonacci_series.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function fibonacci_series(n) {
+    var fbnc = new Array();
+    fbnc[0] = 0;
+    fbnc[1] = 1;
+    var num = 2;
+    while(num<n){
+        fbnc[num] = fbnc[num-1]+fbnc[num-2];
+        num++;
+    }
+    return fbnc;
+}
+
+module.exports = fibonacci_series;
